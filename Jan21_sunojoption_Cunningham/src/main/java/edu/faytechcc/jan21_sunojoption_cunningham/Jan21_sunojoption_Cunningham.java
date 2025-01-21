0e1cc24fb6dfc500c21fc4ecfbe9bb2fbac6b68d
cunning6423 <cunningg4263@student.faytechcc.edu>
2025-01-21 14:40:13 -0500

    Add files via upload
    1/21/25 code

--- Jan21_sunojoption_Cunningham/src/main/java/edu/faytechcc/jan21_sunojoption_cunningham/Jan21_sunojoption_Cunningham.java
+++ Jan21_sunojoption_Cunningham/src/main/java/edu/faytechcc/jan21_sunojoption_cunningham/Jan21_sunojoption_Cunningham.java
@@ -13,8 +13,12 @@ public class Jan21_sunojoption_Cunningham {
 
     public static void main(String[] args) {
         // say hello in JOptionPane
-            JOptionPane.showMessageDialog(null, "Hello, World!","Greeting",JOptionPane.QUESTION_MESSAGE);
-            String firstGrade = JOptionPane.showInputDialog(null, "Enter a song genre: ");   
-            String secondGrade= JOptionPane.showInputDialog(null, "What's anohther song genre?");  
+            JOptionPane.showMessageDialog(null, "Lets make a song!","Suno Helper",JOptionPane.INFORMATION_MESSAGE);
+            String firstGenre = JOptionPane.showInputDialog(null, "Enter a song genre: ");   
+            String secondGenre = JOptionPane.showInputDialog(null, "What's anohther song genre?");  
+            String mix = firstGenre +", "+ secondGenre;
+            // String mix secondGrenre + firstGenre
+            JOptionPane.showMessageDialog(null,mix, "Here is your custom mix", JOptionPane.INFORMATION_MESSAGE);
     }
+    // You could make other methods or function and it will be easier to copy.
 }
