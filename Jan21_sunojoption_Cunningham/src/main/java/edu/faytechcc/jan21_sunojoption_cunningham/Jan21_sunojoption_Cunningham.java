c573974ccb52fb618715e33ec2a327f0d9c459e3
cunning6423 <cunningg4263@student.faytechcc.edu>
2025-01-21 13:49:43 -0500

    Add files via upload

---
+++ Jan21_sunojoption_Cunningham/src/main/java/edu/faytechcc/jan21_sunojoption_cunningham/Jan21_sunojoption_Cunningham.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.faytechcc.jan21_sunojoption_cunningham;
+
+/**
+ *
+ * @author cunningg4263
+ */
+import javax.swing.JOptionPane;
+public class Jan21_sunojoption_Cunningham {
+
+    public static void main(String[] args) {
+        // say hello in JOptionPane
+            JOptionPane.showMessageDialog(null, "Hello, World!","Greeting",JOptionPane.QUESTION_MESSAGE);
+            String firstGrade = JOptionPane.showInputDialog(null, "Enter a song genre: ");   
+            String secondGrade= JOptionPane.showInputDialog(null, "What's anohther song genre?");  
+    }
+}
