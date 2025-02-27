bf3b872e36ad8e38cf26d33f487c3b5fdc5ed4b0
cunning6423 <cunningg4263@student.faytechcc.edu>
2025-02-27 18:46:14 +0000

    Switch Methods and loops _Started

---
+++ Free_Work/Structures_02_28_2025.java
@@ -0,0 +1,46 @@
+//This could hows examples of switches, Methods,  Loops
+import java.util.Scanner;
+public class Structures_02_28_2025 
+{
+    public static void Variables() 
+    {
+        Scanner in = new Scanner(system.in);
+        int goAgain2, goAgain;
+    }
+    public static void main(String[] args) 
+    {
+        System.out.print("\nEnter the number of times you want to run: ");
+        goAgain2 = in.next();
+        for ( int i = 0; i< goAgain2; i++)
+        {
+
+        }    
+    }
+    public static void monthSwitch()
+    {
+        int day = 4;
+switch (day) {
+    case 1:
+        System.out.println("Monday");
+        break;
+    case 2:
+        System.out.println("Tuesday");
+        break;
+    case 3:
+        System.out.println("Wednesday");
+        break;
+    case 4:
+        System.out.println("Thursday");
+        break;
+    case 5:
+        System.out.println("Friday");
+        break;
+    case 6:
+        System.out.println("Saturday");
+        break;
+    case 7:
+        System.out.println("Sunday");
+        break;
+}
+    }
+}
